51b01073467bf5144515155cc3804879093c96f8
Minh Quân <92730976+mnhquan@users.noreply.github.com>
2026-04-16 21:56:41 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { Board } from './core/Board.ts';
 
 function initGame() {
-  // 1. Khởi tạo dữ liệu game (Bộ nhớ)
   const board = new Board();
   
-  // TODO: 2. Khởi tạo giao diện đồ họa (BoardRenderer) tại đây để nối với DOM
+  // TODO: 2. Khởi tạo giao diện đồ họa tại đây để nối với DOM
 
-  // TODO: 3. Khởi tạo bộ máy kiểm tra luật (RuleEngine) & AI Controller
+  // TODO: 3. Khởi tạo bộ máy kiểm tra luật & AI Controller
 
   console.log('[System] Xiangqi logic engine initialized.');
   console.log('[System] Board loaded pieces:', board.pieces.size);
